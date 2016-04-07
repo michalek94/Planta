aa9066922730278933811a8a7b491edf2c98c05b
jako14 <ja.ko14@wp.pl>
2016-04-07 07:53:38 +0200

    Activity Janusza

---
+++ app/src/main/java/pl/planta/activity/JanuszWorkActivity.java
@@ -0,0 +1,37 @@
+package pl.planta.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import pl.planta.R;
+
+/**
+ * Created by Jakub on 2016-04-06.
+ */
+public class JanuszWorkActivity extends Activity {
+
+    private ImageButton heapButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_januszw);
+
+        heapButton = (ImageButton)findViewById(R.id.heapButton);
+
+       heapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameIntent = new Intent(JanuszWorkActivity.this, GamingTestsActivity.class);
+                startActivity(gameIntent);
+            }
+        });
+
+    }
+
+}
