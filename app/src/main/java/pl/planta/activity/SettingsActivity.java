5301ea8669ac384eb0bb0dc47727aaad5bde4a64
michalek94 <pankos30@gmail.com>
2016-04-07 23:12:49 +0200

    PreferenceActivity - testing

---
+++ app/src/main/java/pl/planta/activity/SettingsActivity.java
@@ -0,0 +1,37 @@
+package pl.planta.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.WindowManager;
+
+import pl.planta.R;
+
+public class SettingsActivity extends PreferenceActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /**
+         * Set flags to make full screen on device
+         */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        /**
+         *
+         */
+        getFragmentManager().beginTransaction().add(android.R.id.content,
+                new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment{
+        private final static String TAG = "UserPreferencesFragment";
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Log.i(TAG, "fragment onCreate");
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
