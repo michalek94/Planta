f6bcf79ded1591d7628368923ae514da9f7bd238
jako14 <ja.ko14@wp.pl>
2016-04-28 20:49:45 +0200

    Pop up zrobiony*

--- app/src/main/java/pl/planta/activity/PlantaActivity.java
+++ app/src/main/java/pl/planta/activity/PlantaActivity.java
@@ -1,16 +1,16 @@
 package pl.planta.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.*;
-
-import pl.planta.activity.PopUp;
 
 
 import pl.planta.R;
@@ -18,7 +18,11 @@ import pl.planta.R;
 public class PlantaActivity extends Activity  {
 
 
-    ImageButton but;
+    private ImageButton but;
+    private PopupWindow popUp;
+    private LayoutInflater layoutInflater;
+    private android.support.percent.PercentRelativeLayout relativeLayout;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +31,39 @@ public class PlantaActivity extends Activity  {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_planta);
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        final int width = dm.widthPixels;
+        final int height = dm.heightPixels;
+
+        relativeLayout = (android.support.percent.PercentRelativeLayout) findViewById(R.id.plantaLayout);
+
         but = (ImageButton)findViewById(R.id.furnaceButton);
 
         but.setOnClickListener(new OnClickListener() {
-            @Override
             public void onClick(View v) {
 
 
-                //PopUp.change(20,20,"test");
+                layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popwindow,null);
+                popUp = new PopupWindow(container,400,400,true);
+                final int widthFur =(int) (width * .7);
+                final int heightFur =(int) (height * .35);
+                popUp.showAtLocation(relativeLayout, Gravity.NO_GRAVITY,widthFur, heightFur);
 
-                startActivity(new Intent(PlantaActivity.this, PopUp.class));
+
+                container.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+
+                        popUp.dismiss();
+                        return false;
+                    }
+                });
 
             }
+
         });
-
-
-
     }
 }
